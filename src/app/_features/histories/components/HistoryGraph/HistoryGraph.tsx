09c777fc4e64a280a9808eb1854501093d5def8f
ruritoBlogger <ruriro0125@gmail.com>
2023-10-29 20:13:58 +0900

    :sparkles: DB のデータをもとにグラフをなんとなく表示

--- src/app/_features/histories/components/HistoryGraph/HistoryGraph.tsx
+++ src/app/_features/histories/components/HistoryGraph/HistoryGraph.tsx
@@ -1,4 +1,28 @@
+"use client"
+
 import React from "react";
+import {Line} from "react-chartjs-2";
+import {
+  CategoryScale,
+  Chart as ChartJS,
+  ChartOptions,
+  Legend,
+  LinearScale,
+  LineElement,
+  PointElement,
+  Title,
+  Tooltip
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 export const HistoryGraph: React.FC = async () => {
   const res = await fetch(
@@ -7,17 +31,34 @@ export const HistoryGraph: React.FC = async () => {
   const json = await res.json();
   const histories = json.data;
 
+  const options: ChartOptions<"line"> = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const,
+      },
+      title: {
+        display: true,
+        text: 'History Graph'
+      },
+    },
+  }
+
+  const data: any = {
+    labels: histories.map((history) => history.date),
+    datasets: [
+      {
+        label: "test",
+        data: histories.map((history) => history.score),
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      }
+    ]
+  }
+
   return (
     <>
-      <p>history graph</p>
-      {histories.map((history) => (
-        <div key={history.id}>
-          <p>id: {history.id}</p>
-          <p>date: {history.date}</p>
-          <p>score: {history.score}</p>
-        </div>
-      ))
-      }
+      <Line options={options} data={data}/>
     </>
   )
 }
